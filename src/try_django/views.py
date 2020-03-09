02eee52d6e19343a853d826e50f7d53e3be81194
ricardo <ricardo85x@gmail.com>
2020-03-09 10:56:47 -0300

    7 - Multiple views

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -2,3 +2,11 @@ from django.http import HttpResponse
 
 def home_page(request):
     return HttpResponse("<h1>Ola mundo</h1>")
+
+
+def about_page(request):
+    return HttpResponse("<h1>About Page</h1>")
+
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Page</h1>")
